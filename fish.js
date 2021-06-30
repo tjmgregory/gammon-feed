d438ae7be77d11dc4f8772868c03e9cc502339b5
Theo Gregory <tjmg@outlook.com>
2021-06-30 16:02:21 +0100

    Fully working federated graphql apollo gateway

--- fish.js
+++ fish.js
@@ -5,30 +5,46 @@ const PORT = 4002
 
 const typeDefs = gql`
     type Fish {
+        id: ID!
         name: String
         colour: String
+        sea: Sea
     }
 
-    type Query {
-        fish: [Fish]
+    extend type Sea @key(fields: "id") {
+        id: ID! @external
+    }
+
+    extend type Query {
+        fish(id: ID!): Fish
+        # It pains me to have to pluralise fish, but alas fishies is cutey
+        fishies: [Fish]
     }
 `
 
 const fish = [
     {
+        id: '1',
         name: 'Shark',
         colour: 'Grey',
+        sea: '2',
     },
     {
+        id: '2',
         name: 'Baracuda',
         // Almost certainly wrong, but I'm not researching fish colours for the sake of some data
         colour: 'Yellow',
+        sea: '2',
     },
 ]
 
 const resolvers = {
+    Fish: {
+        sea: (fish) => ({ __typename: 'Sea', id: fish.sea }),
+    },
     Query: {
-        fish: () => fish,
+        fish: (_, { id }) => fish.find((fish) => fish.id === id),
+        fishies: () => fish,
     },
 }
 
@@ -37,5 +53,5 @@ const server = new ApolloServer({
 })
 
 server.listen({ port: PORT }).then(({ url }) => {
-    console.log(`🐠 Fishes officially swimming at ${url}.`)
+    console.log(`🐠 O-fish-ally swimming at ${url}.`)
 })
--- seas.js
+++ seas.js
@@ -4,29 +4,37 @@ const { gql, ApolloServer } = require('apollo-server')
 const PORT = 4001
 
 const typeDefs = gql`
-    type Sea {
+    type Sea @key(fields: "id") {
+        id: ID!
         name: String
         volume: Int
     }
 
-    type Query {
+    extend type Query {
+        sea(id: ID!): Sea
         seas: [Sea]
     }
 `
 
 const seas = [
     {
+        id: '1',
         name: 'Atlantic',
         volume: 24948,
     },
     {
+        id: '2',
         name: 'Pacific',
         volume: 88888,
     },
 ]
 
 const resolvers = {
+    Sea: {
+        __resolveReference: (ref) => seas.find((sea) => sea.id === ref.id),
+    },
     Query: {
+        sea: (_, { id }) => seas.find((sea) => sea.id === id),
         seas: () => seas,
     },
 }
